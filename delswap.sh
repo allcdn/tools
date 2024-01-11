5dcf40616fc0e2877a91be58dcd8445f3739d885
路人 <70009473+allcdn@users.noreply.github.com>
2024-01-11 11:12:15 +0800

    Update delswap.sh

--- delswap.sh
+++ delswap.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# 关闭所有swap分区
+# 安装sudo工具
 apt install sudo -y
+
+# 关闭所有swap分区
 sudo swapoff -a
 
-# 选择swap分区
-swap_line=$(cat /etc/fstab | grep swap)
-swap_id=$(echo $swap_line | awk '{print $1}')
+# 获取swap分区信息
+swap_line=$(grep "swap" /etc/fstab)
 
-# 如果swap分区被UUID标识
-if [[ $swap_id =~ UUID=(.+) ]]
-then
-    swap_part=$(lsblk -no NAME,UUID | grep ${BASH_REMATCH[1]} | awk '{print $1}')
-    sudo sed -i "/UUID=${BASH_REMATCH[1]}/d" /etc/fstab
+# 检查UUID存在与否
+if [[ $swap_line =~ UUID=(.+)[[:space:]] ]]; then
+  swap_uuid=${BASH_REMATCH[1]}
+  swap_dev=$(findmnt -no SOURCE -U UUID=$swap_uuid)
 else
-    # 否则，swap分区被设备名如/dev/sdaX标识
-    swap_part=${swap_id/\/dev\//}
-    sudo sed -i "/${swap_id/\\/\\\\/}/d" /etc/fstab
+  swap_dev=$(echo $swap_line | awk '{print $1}')
 fi
 
+# 从/etc/fstab中删除swap分区
+sudo sed -i "/swap/d" /etc/fstab
+
 # 删除物理swap分区
-echo -e "d\nw\n" | sudo fdisk /dev/${swap_part::-1}
+swap_dev_prefix=$(echo $swap_dev | rev | cut -c 2- | rev)
+echo -e "d\nw\n" | sudo fdisk $swap_dev_prefix
