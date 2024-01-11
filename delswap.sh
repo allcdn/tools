9be0032272d033ae58c135315a9adc5484f4bdb3
路人 <70009473+allcdn@users.noreply.github.com>
2024-01-11 11:15:37 +0800

    Update delswap.sh
    增加lvm

--- delswap.sh
+++ delswap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 安装sudo工具
-apt install sudo -y
+# 安装必要的工具
+apt install sudo lvm2 -y
 
 # 关闭所有swap分区
 sudo swapoff -a
@@ -11,15 +11,19 @@ swap_line=$(grep "swap" /etc/fstab)
 
 # 检查UUID存在与否
 if [[ $swap_line =~ UUID=(.+)[[:space:]] ]]; then
-  swap_uuid=${BASH_REMATCH[1]}
-  swap_dev=$(findmnt -no SOURCE -U UUID=$swap_uuid)
+    swap_uuid=${BASH_REMATCH[1]}
+    swap_dev=$(findmnt -no SOURCE -U UUID=$swap_uuid)
 else
-  swap_dev=$(echo $swap_line | awk '{print $1}')
+    swap_dev=$(echo $swap_line | awk '{print $1}')
 fi
 
 # 从/etc/fstab中删除swap分区
 sudo sed -i "/swap/d" /etc/fstab
 
 # 删除物理swap分区
-swap_dev_prefix=$(echo $swap_dev | rev | cut -c 2- | rev)
-echo -e "d\nw\n" | sudo fdisk $swap_dev_prefix
+if [[ $swap_dev =~ /dev/mapper/(.+) ]]; then  # LVM partition
+    sudo lvremove -y ${BASH_REMATCH[0]}
+else
+    swap_dev_prefix=$(echo $swap_dev | rev | cut -c 2- | rev)
+    echo -e "d\nw\n" | sudo fdisk $swap_dev_prefix
+fi
