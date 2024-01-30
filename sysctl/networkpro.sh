e0896959c00c16ff50eddb17d11da6244d5b7a97
路人 <70009473+allcdn@users.noreply.github.com>
2024-01-30 14:12:42 +0800

    Update networkpro.sh

--- sysctl/networkpro.sh
+++ sysctl/networkpro.sh
@@ -16,15 +16,15 @@ net.ipv4.tcp_notsent_lowat = 131072
 # 本地端口的可用范围（最小值到最大值）
 net.ipv4.ip_local_port_range = 1024 65535
 # 允许的 TCP 和 UDP 读缓冲区最大值
-net.core.rmem_max = 536870912
+net.core.rmem_max = 16777216
 # 允许的 TCP 和 UDP 写缓冲区最大值
-net.core.wmem_max = 536870912
+net.core.wmem_max = 16777216
 # socket 监听队列的最大长度，适合大量并发连接的场景
-net.core.somaxconn = 32768
+net.core.somaxconn = 65535
 # 网络设备队列的最大数据包数量
 net.core.netdev_max_backlog = 5000
 # TIME-WAIT 状态下的 socket 最大数量，防止 TIME-WAIT 锁占用资源
-net.ipv4.tcp_max_tw_buckets = 65536
+net.ipv4.tcp_max_tw_buckets = 1440000
 # 当 TCP 接收缓冲溢出时是否立即放弃连接
 net.ipv4.tcp_abort_on_overflow = 1
 # TCP 空闲时是否禁用慢启动
@@ -40,7 +40,7 @@ net.ipv4.tcp_synack_retries = 3
 # 等待建立连接的 SYN 请求队列的最大长度
 net.ipv4.tcp_max_syn_backlog = 32768
 # TCP 连接在 FIN-WAIT-2 状态下存活的时间
-net.ipv4.tcp_fin_timeout = 15
+net.ipv4.tcp_fin_timeout = 10
 # 重用处于 TIME_WAIT 状态的socket
 net.ipv4.tcp_tw_reuse = 1
 # 开启快速回收 TIME_WAIT 状态的套接字
@@ -71,6 +71,12 @@ net.ipv4.tcp_fastopen = 3
 vm.swappiness=0
 # 避免发送小的数据包，即启用 Nagle's algorithm
 net.ipv4.tcp_nodelay = 1
+# 开启反向路径过滤
+net.ipv4.conf.all.rp_filter = 1
+net.ipv4.conf.default.rp_filter = 1
+# 允许较大的队列长度
+net.netfilter.nf_conntrack_max = 1048576
+net.nf_conntrack_max = 1048576
 EOF
 
 sysctl -p
